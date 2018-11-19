165748cee90a1023076078e1dfd589a8e39b4596
abeskrovna <abeskrovna@gmail.com>
2018-11-18 22:21:53 -0500

    Implemented subtraction and worked on printing

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ def addition(left, right):
     sum = left + right
     return sum
 
-result = addition(2,3)
-print(result)
+def subtraction(left, right):
+    difference = left - right
+    return difference
+
+sum = addition(2,3)
+difference = subtraction(4,5)
+
+print(sum, difference)
+
+print("sum: " + str(sum) + " difference: " + str(difference))
+
+print(f"sum: {sum} difference: {difference}")
